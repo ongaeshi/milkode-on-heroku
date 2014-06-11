32592896f2204488f4068402c3b81edd3809664e
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 01:38:21 +0900

    Rewrite milkode.yaml

--- groonga/init.rb
+++ groonga/init.rb
@@ -1,16 +1,14 @@
 require 'milkode/cli'
 require 'fileutils'
 
-ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '../../.milkode')
+root_dir = File.expand_path(File.join(ENV['GROONGA_DATABASE_PATH'], '../..'))
+ENV['MILKODE_DEFAULT_DIR'] = File.join(root_dir, '.milkode')
 
 CLI.start("init".split)
 
 open("./PACKAGES") do |f|
-  dst_dir = File.expand_path(File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git"))
-  p dst_dir
+  dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
   FileUtils.mkdir_p dst_dir
-  # dst_dir = File.join("/app/packages/git")
-  # FileUtils.mkdir_p dst_dir
   
   f.each do |url|
     url.chomp!
@@ -19,4 +17,17 @@ open("./PACKAGES") do |f|
     system("git clone --depth 1 #{url} #{filename}")
     CLI.start("add #{filename}".split)
   end
+end
+
+milkode_yaml = File.join(ENV['MILKODE_DEFAULT_DIR'], "milkode.yaml")
+str = ""
+
+open(milkode_yaml) do |f|
+  f.each do |line|
+    str += line.sub(/#{root_dir}/, "/app")
+  end
+end
+
+open(milkode_yaml, "w") do |f|
+  f.write(str)
 end
