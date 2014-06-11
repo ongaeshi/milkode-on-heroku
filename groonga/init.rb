01a42dd14330666a1639ea779000124999d6b090
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 01:26:13 +0900

    expand_path

--- groonga/init.rb
+++ groonga/init.rb
@@ -6,11 +6,11 @@ ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '../../.mil
 CLI.start("init".split)
 
 open("./PACKAGES") do |f|
-  # dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
-  # p dst_dir
-  # FileUtils.mkdir_p dst_dir
-  dst_dir = File.join("/app/packages/git")
+  dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git").expand_path
+  p dst_dir
   FileUtils.mkdir_p dst_dir
+  # dst_dir = File.join("/app/packages/git")
+  # FileUtils.mkdir_p dst_dir
   
   f.each do |url|
     url.chomp!
