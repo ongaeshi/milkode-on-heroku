c785baeb32ace6b1c11b0b66145e13e077b04621
ongaeshi <ongaeshi0621@gmail.com>
2014-06-11 23:35:15 +0900

    test

--- groonga/init.rb
+++ groonga/init.rb
@@ -2,6 +2,10 @@ require 'milkode/cli'
 
 p ENV['GROONGA_DATABASE']
 p ENV['GROONGA_DATABASE_PATH']
+p ENV['HOME']
+
+ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '..')
+p ENV['MILKODE_DEFAULT_DIR']
 
 CLI.start("init".split)
 CLI.start("add --from-file ./PACKAGES".split)
