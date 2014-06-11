f627d1ee29ab1faad8f9cdcc0bee9f62ceff348c
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 01:54:07 +0900

    Change PACKAGES rule

--- groonga/init.rb
+++ groonga/init.rb
@@ -6,28 +6,35 @@ ENV['MILKODE_DEFAULT_DIR'] = File.join(root_dir, '.milkode')
 
 CLI.start("init".split)
 
+milkode_yaml_str = <<EOF
+---
+version: '0.2'
+contents:
+EOF
+
 open("./PACKAGES") do |f|
   dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
   FileUtils.mkdir_p dst_dir
   
-  f.each do |url|
-    url.chomp!
+  f.each do |path|
+    path.chomp!
+    url = "https://github.com/#{path}.git"
     name = File.basename(url).sub(/\.git\Z/, "")
     filename = File.join(dst_dir, name)
+
     system("git clone --depth 1 #{url} #{filename}")
     CLI.start("add #{filename}".split)
+
+milkode_yaml_str += <<EOF
+- directory: #{filename}
+  options:
+    :github: #{path}
+EOF
   end
 end
 
 milkode_yaml = File.join(ENV['MILKODE_DEFAULT_DIR'], "milkode.yaml")
-str = ""
-
-open(milkode_yaml) do |f|
-  f.each do |line|
-    str += line.sub(/#{root_dir}/, "/app")
-  end
-end
 
 open(milkode_yaml, "w") do |f|
-  f.write(str)
+  f.write(milkode_yaml_str)
 end
