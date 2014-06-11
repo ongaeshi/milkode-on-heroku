38fb2441b7da60398d8c8c45511ca373e4d080cc
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 01:18:04 +0900

    git clone --depath 1

--- groonga/init.rb
+++ groonga/init.rb
@@ -4,9 +4,16 @@ require 'fileutils'
 ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '../../.milkode')
 
 CLI.start("init".split)
-CLI.start("add --from-file ./PACKAGES".split)
 
-p File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages')
-system("rm -rf #{File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages')}")
-
-# FileUtils.rm_rf(File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages'))
+open("./PACKAGES") do |f|
+  dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
+  FileUtils.mkdir_p dst_dir
+  
+  f.each do |url|
+    url.chomp!
+    name = File.basename(url).sub(/\.git\Z/, "")
+    filename = File.join(dst_dir, name)
+    system("git clone --depth 1 #{url} #{filename}")
+    CLI.start("add #{filename}".split)
+  end
+end
