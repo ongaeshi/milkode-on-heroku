f5f4ce42aebb48b667032a0cb5ff441b9902b823
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 00:24:17 +0900

    test

--- groonga/init.rb
+++ groonga/init.rb
@@ -6,4 +6,6 @@ ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '../../.mil
 CLI.start("init".split)
 CLI.start("add --from-file ./PACKAGES".split)
 
+p File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages')
+
 FileUtils.rm_rf(File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages'))
