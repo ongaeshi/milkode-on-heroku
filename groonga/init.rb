0e4de73660f09452b92acb3d56462a50cf7f01d3
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 00:00:20 +0900

    rm -rf packages

--- groonga/init.rb
+++ groonga/init.rb
@@ -1,6 +1,9 @@
 require 'milkode/cli'
+require 'fileutis'
 
 ENV['MILKODE_DEFAULT_DIR'] = File.join(ENV['GROONGA_DATABASE_PATH'], '../../.milkode')
 
 CLI.start("init".split)
 CLI.start("add --from-file ./PACKAGES".split)
+
+FileUtils.rm_rf(File.join(ENV['MILKODE_DEFAULT_DIR'], 'packages'))
