a69d9b75446a43d3e5ab021b106d3e997836cf8d
ongaeshi <ongaeshi0621@gmail.com>
2014-06-12 23:24:56 +0900

    Add AddPackage class

--- groonga/init.rb
+++ groonga/init.rb
@@ -1,31 +1,33 @@
 require 'milkode/cli'
 require 'fileutils'
 
-root_dir = File.expand_path(File.join(ENV['GROONGA_DATABASE_PATH'], '../..'))
-ENV['MILKODE_DEFAULT_DIR'] = File.join(root_dir, '.milkode')
+class AddPackage
+  attr_reader :dst_dir
+  attr_reader :path
 
-CLI.start("init".split)
+  def initialize(dst_dir, str)
+    @dst_dir = dst_dir
+    @path = str
+  end
 
-milkode_yaml_str = <<EOF
----
-version: '0.2'
-contents:
-EOF
+  def url
+    "https://github.com/#{path}.git"
+  end
 
-open("./PACKAGES") do |f|
-  dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
-  FileUtils.mkdir_p dst_dir
-  
-  f.each do |path|
-    path.chomp!
-    url = "https://github.com/#{path}.git"
-    name = File.basename(url).sub(/\.git\Z/, "")
-    filename = File.join(dst_dir, name)
+  def name
+    File.basename(url).sub(/\.git\Z/, "")
+  end
 
-    system("git clone --depth 1 #{url} #{filename}")
-    CLI.start("add #{filename}".split)
+  def filename
+    File.join(dst_dir, name)
+  end
 
-milkode_yaml_str += <<EOF
+  def git_clone
+    "git clone --depth 1 #{url} #{filename}"
+  end
+
+  def yaml
+    <<EOF
 - directory: #{filename}
   options:
     :github: #{path}
@@ -33,8 +35,29 @@ EOF
   end
 end
 
-milkode_yaml = File.join(ENV['MILKODE_DEFAULT_DIR'], "milkode.yaml")
+# ---
+root_dir = File.expand_path(File.join(ENV['GROONGA_DATABASE_PATH'], '../..'))
+ENV['MILKODE_DEFAULT_DIR'] = File.join(root_dir, '.milkode')
+milkode_yaml_str = <<EOF
+---
+version: '0.2'
+contents:
+EOF
 
-open(milkode_yaml, "w") do |f|
+CLI.start("init".split)
+
+open("./PACKAGES") do |f|
+  dst_dir = File.join(ENV['MILKODE_DEFAULT_DIR'], "packages/git")
+  FileUtils.mkdir_p dst_dir
+  
+  f.each do |path|
+    package = AddPackage.new(dst_dir, path.chomp)
+    system(package.git_clone)
+    CLI.start("add #{package.filename}".split)
+    milkode_yaml_str += package.yaml
+  end
+end
+
+open(File.join(ENV['MILKODE_DEFAULT_DIR'], "milkode.yaml"), "w") do |f|
   f.write(milkode_yaml_str)
 end
